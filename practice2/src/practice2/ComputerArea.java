1857ef538fb768affa310e84c031c4f36a9d7b8a
Jiacheng Wei <982957368@qq.com>
2018-03-24 16:36:02 +0800

    2018.3.24java练习

---
+++ practice2/src/practice2/ComputerArea.java
@@ -0,0 +1,16 @@
+package practice2;
+
+public class ComputerArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		double i=50.0;
+		double k=i+50.0;
+		double j=k+i;
+		
+		System.out.println("j is"+j+"and"+
+				"k is"+k);
+
+	}
+
+}
