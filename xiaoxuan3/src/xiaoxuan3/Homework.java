8dc7782fbb02e3ef6c661e6e8fab094f8c1f28e9
Jiacheng Wei <982957368@qq.com>
2018-05-07 14:47:19 +0800

    2018.5.7

---
+++ xiaoxuan3/src/xiaoxuan3/Homework.java
@@ -0,0 +1,16 @@
+package xiaoxuan3;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1,sum=0;
+		while (sum<8888)
+		{
+			sum=sum+i;
+			i=i+1;
+		}
+		System.out.println(i-2);
+	}
+
+}
